10006cd2d8b1255f78ecd4525018f041efb8594b
EmiyaHana <warm310149@gmail.com>
2026-04-21 16:24:26 +0700

    Chain_Spike_Enemy_SetUp

--- Assets/Script/Player2DController.cs
+++ Assets/Script/Player2DController.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player2DController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5.0f;
+    public float jumpForce = 450f;
+
+    private bool _isGrounded = false;
+    private float moveValue;
+    private Rigidbody2D _rb;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current != null)
+        {
+            moveValue = (Keyboard.current.dKey.isPressed ? 1 : 0) - (Keyboard.current.aKey.isPressed ? 1 : 0);
+        }
+        _rb.linearVelocity = new Vector2(moveValue * speed, _rb.linearVelocity.y);
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            _rb.AddForce(new Vector2(_rb.linearVelocity.x, jumpForce));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = false;
+        }
     }
 }
